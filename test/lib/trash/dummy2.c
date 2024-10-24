43ed196c6bc06a0797b82097dd29eb3e925ca712
ylllc <leprechaun@yggdrasil-leaves.com>
2024-10-24 15:41:30 +0000

    remove dummy files

--- test/lib/trash/dummy2.c
+++
@@ -1,7 +0,0 @@
-// [CAUTION]
-// It's a dummy file for test TestRunner 
-// and will be deleted. 
-#include "trash/dummy.h"
-
-int test_always(){return test_lib_func();}
-int test_never(){return 1;}
