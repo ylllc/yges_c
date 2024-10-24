1e9ecff79d237856df24045aed4494b05d41278b
ylllc <leprechaun@yggdrasil-leaves.com>
2024-10-24 15:29:27 +0000

    add memory emergency

--- api/memory/safe_alloc.h
+++ api/memory/safe_alloc.h
@@ -2,12 +2,27 @@
 /*!	@brief  Safety Memory Allocator
 	@author  Copyright © 2024 Yggdrasil Leaves, LLC.
 	@sa  https://github.com/ylllc/yges_c
+
+	@pre  define YGES_TRAP_CANTALLOC before include this
+		to hook to report emergency
 */
 #ifndef YGES_SAFE_ALLOC_H__
 #define YGES_SAFE_ALLOC_H__
 
 #include <stddef.h>
 #include <stdlib.h>
+
+//! emergency info for Out of Memory 
+typedef struct YgEsEmergencyInfo_OutOfMemory_ YgEsEmergencyInfo_OutOfMemory;
+
+//! Emergency cause: Out of Memory 
+extern const char* YgEsEmergencyCause_OutOfMemory;
+
+
+struct YgEsEmergencyInfo_OutOfMemory_{
+	size_t size; //!< trying size 
+};
+
 
 //! safety memory freer 
 /*!	@param memp  target var pointer of stored instance pointer
@@ -20,17 +35,42 @@
 		} \
 	}
 
+#ifdef YGES_TRAP_CANTALLOC
+#define yges_quick_alloc(size) yges_quick_alloc_trap(size,__FILE__,__LINE__)
+#define yges_safe_alloc(size) yges_safe_alloc_trap(size,__FILE__,__LINE__)
+#else
+
+//! quick memory allocator 
+/*!	@param size  memory size
+	@return  allocated memory block (or NULL)
+	@note  allocate a dirty memory block. @n
+		you must fully initialize it. @n
+	@note  define YGES_TRAP_CANTALLOC before include this
+		to hook to report emergency
+*/
+#define yges_quick_alloc(size) malloc(size)
+
+/*!	@param size  memory size
+	@return  allocated memory block (or NULL)
+	@note  allocate a memory block and clear to zero
+	@note  define YGES_TRAP_CANTALLOC before include this
+		to hook to report emergency
+*/
+#define yges_safe_alloc(size) yges_safe_alloc_thru(size)
+
+#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-//! safety mamory allocator 
-/*!	@param size  memory size
-	@return  allocated memory block (or NULL)
-	@note  allocate a memory block and clear to zero
-*/
-void* yges_safe_alloc(size_t size);
+//! trappable yges_quick_alloc() 
+void* yges_quick_alloc_trap(size_t size,const char* file,int line);
+
+//! untrappable yges_safe_alloc() 
+void* yges_safe_alloc_thru(size_t size);
+//! trappable yges_safe_alloc() 
+void* yges_safe_alloc_trap(size_t size,const char* file,int line);
 
 #ifdef __cplusplus
 }
---
+++ test/scn/memory/memory_emergency.c
@@ -0,0 +1,59 @@
+﻿/* † Yggdrasil Essence † */
+/*!	@brief  Memory Emergency Test
+	@author  Copyright © 2024 Yggdrasil Leaves, LLC.
+	@sa  https://github.com/ylllc/yges_c
+*/
+#include "system/emergency_trap.h"
+#include <stdio.h>
+#include <string.h>
+
+// trap enabled 
+#define YGES_TRAP_CANTALLOC
+#include "memory/safe_alloc.h"
+
+static int ExitCode_Failure=1;
+static int ExitCode_Emergency=74;
+
+//! test emergency context 
+static YgEsEmergencyContext TestEmergencyContext;
+
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+static int on_emergency_ctx(YgEsEmergencyContext* ctx,const char* cause,void* info,const char* file,int line){
+
+fprintf(stderr,"emergency: %s (%s:%d)\n",cause,file,line);
+
+	// ignore from other causes 
+	if(cause!=YgEsEmergencyCause_OutOfMemory)return 0;
+
+	// will be exited 
+	return ExitCode_Emergency;
+}
+
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+int main(int argc,char** argv){
+
+	// Emergency settings 
+	TestEmergencyContext.Enabled=false;
+	TestEmergencyContext.Hook=on_emergency_ctx;
+	TestEmergencyContext.User=NULL;
+	yges_register_emergency(&TestEmergencyContext);
+
+	// generally 
+	ExitCode_Emergency=75;
+	void* p=yges_quick_alloc(100);
+	yges_safe_free(&p);
+
+	// should be null; trap is disabled 
+	ExitCode_Emergency=76;
+	p=yges_quick_alloc(-1);
+	if(p)return ExitCode_Failure;
+
+	// should be trapped 
+	ExitCode_Emergency=77;
+	TestEmergencyContext.Enabled=true;
+	yges_safe_alloc(-1);
+
+	// should not step this 
+	ExitCode_Emergency=78;
+	return ExitCode_Failure;
+}
